156455e71287d671b509419896de997248359a3f
Brady Buchholz <bradybuchholz@gmail.com>
2021-12-08 18:21:39 -0500

    Adjusted the manifest, and added the activate function to the service worker

--- Develop/service-worker.js
+++ Develop/service-worker.js
@@ -1,25 +1,39 @@
+const CACHE_NAME = "static-cache-v2";
+const DATA_CACHE_NAME = "data-cache-v1";
+
 // install event handler
 self.addEventListener('install', event => {
-    event.waitUntil(
-      caches.open('static').then( cache => {
-        return cache.addAll([
-          './',
-          './public/index.html',
-          './public/styles.css',
-          './public/icons/icon-192x192.png',
-          './public/icons/icon-512x512.png',
-        ]);
+  event.waitUntil(
+    caches.open('static').then( cache => {
+      return cache.addAll([
+        './',
+        '/index.js', 
+        'index.html',
+        'styles.css',
+        "/icons/icon-192x192.png", 
+        "/icons/icon-512x512.png"
+      ]);
+    })
+  );
+  console.log('Install');
+  self.skipWaiting();
+});
+
+  // activate
+  self.addEventListener("activate", function (evt) {
+    evt.waitUntil(
+      caches.keys().then(keyList => {
+        return Promise.all(
+          keyList.map(key => {
+            if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+              console.log("Removing old cache data", key);
+              return caches.delete(key);
+            }
+          })
+        );
       })
     );
-    console.log('Install');
-    self.skipWaiting();
+  
+    self.clients.claim();
   });
   
-  // retrieve assets from cache
-  self.addEventListener('fetch', event => {
-    event.respondWith(
-      caches.match(event.request).then( response => {
-        return response || fetch(event.request);
-      })
-    );
-  });
